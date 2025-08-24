14e6fe77ca0ae8eb0f9ce4242510cc3fc5942698
diforza <68056908+diforza@users.noreply.github.com>
2025-08-24 21:10:37 +0500

    remove_logo.js

--- remove_logo.js
+++ remove_logo.js
@@ -1,5 +1,5 @@
 (function () {
-    'use 'use strict';
+    'use strict';
 
     /**
      * Функция для удаления элемента логотипа.
@@ -19,7 +19,7 @@
     function initPlugin() {
         Lampa.Plugin.create({
             name: 'Logo Remover (Persistent)',
-            version: '1.2.0', // Обновляем версию
+            version: '1.2.1', // Обновляем версию
             author: 'User',
             
             start: function () {
@@ -37,7 +37,7 @@
                 // --- Метод 2: Постоянный наблюдатель (MutationObserver) ---
                 // Это надежный запасной вариант, который отловит любые изменения на странице.
                 // Он не будет отключаться и будет удалять логотип каждый раз при его появлении.
-                const observer = new MutationObserver((mutations) => {
+                const observer = new MutationObserver(() => {
                     // Просто проверяем, не появился ли логотип снова
                     if (document.querySelector('.head__logo-icon')) {
                         removeLogo();
